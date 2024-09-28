78a77bc7b180eb4068eec32631d2809a84d20d31
v1negret <maks.bezruchkin33@gmail.com>
2024-09-28 19:51:02 +0300

    ExcelService_WriteAsOpenXml_ShouldContainsCorrectContent Unit test added

--- MonitorElectric.Tests/ExcelServiceTest.cs
+++ MonitorElectric.Tests/ExcelServiceTest.cs
@@ -29,4 +29,40 @@ public class ExcelServiceTest
         Assert.True(File.Exists(path));
         File.Delete(path);
     }
+    
+    
+    //Данный тест выглядит не очень красиво и правильно, но я подумал, что он необходим
+    [Fact]
+    public async Task ExcelService_WriteAsOpenXml_ShouldContainsCorrectContent()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "ExcelFile.xlsx");
+        var items = new List<RssItem>()
+        {
+            new RssItem()
+            {
+                Author = "John Doe", Description = "Description 1", Link = "https://", PublishDate = DateTime.Now,
+                Title = "Title 1"
+            }
+        };
+        var excelService = new ExcelService();
+        await excelService.WriteAsOpenXml(path, items);
+        
+        var excelPackage = new ExcelPackage(new FileInfo(path));
+        var worksheet = excelPackage.Workbook.Worksheets["RssExcel"];
+        
+        Assert.Equal(worksheet.Cells[1,1].Value, "Заголовок");
+        Assert.Equal(worksheet.Cells[1,2].Value, "Ссылка");
+        Assert.Equal(worksheet.Cells[1,3].Value, "Дата публикации");
+        Assert.Equal(worksheet.Cells[1,4].Value, "Автор");
+        Assert.Equal(worksheet.Cells[1,5].Value, "Описание");
+        Assert.Equal(worksheet.Cells[2,1].Value, "Title 1");
+        Assert.Equal(worksheet.Cells[2,2].Value, "https://");
+        Assert.Equal(worksheet.Cells[2,4].Value, "John Doe");
+        Assert.Equal(worksheet.Cells[2,5].Value, "Description 1");
+        
+        worksheet.Dispose();
+        excelPackage.Dispose();
+        
+        File.Delete(path);
+    }
 }
